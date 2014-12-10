3cd768eb653aa317f9d214bb30c5bb22c76f49f2
twhalen08 <twhalen08@gmail.com>
2014-12-10 13:00:15 -0500

    Assignment 2-2

--- Week3/Assignment2-1/update-exec.php
+++ Week3/Assignment2-1/update-exec.php
@@ -1,15 +1,31 @@
 <?php
+//require the database connection script
 require('database.php');
-$uid = $_POST['userID'];
-$fullname = $_POST['fullname'];
-$email = $_POST['email'];
-$phone = $_POST['phone'];
-$zip = $_POST['zip'];
 
-  require_once('database.php');
-$query = "UPDATE users SET fullname = $fullname, email = $email, phone = $phone, zip = $zip WHERE id = $uid";
-$db->exec($query);
 
-echo $uid;
-echo $fullname;
-echo "Done";
+//filter post data
+$uid = filter_input(INPUT_POST, 'userID');
+$fullname = filter_input(INPUT_POST, 'fullname');
+$email = filter_input(INPUT_POST, 'email');
+$phone = filter_input(INPUT_POST, 'phone');
+$zip = filter_input(INPUT_POST, 'zip');
+
+//Create a paramaterized query
+$qry = $db->prepare('UPDATE users SET fullname=:fullname, email=:email, phone=:phone, zip=:zip WHERE id=:id');
+$qry->bindParam(':id', $uid, PDO::PARAM_INT);
+$qry->bindParam(':fullname', $fullname, PDO::PARAM_INT);
+$qry->bindParam(':phone', $phone, PDO::PARAM_INT);
+$qry->bindParam(':email', $email, PDO::PARAM_INT);
+$qry->bindParam(':zip', $zip, PDO::PARAM_INT);
+
+
+// if the query executes and returns at least one row...
+ if ( $qry->execute() && $qry->rowCount() > 0 ) {
+     //send the user back to the user list. 
+     header('Location: viewUsers.php');
+ } else {
+     //warn the user of the failure and give them the chance to retry
+      echo 'User Update Failed Please <a href="viewUsers.php">try again</a>';
+
+ }     
+?>
